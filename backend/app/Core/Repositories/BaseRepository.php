452f0b93328ce82857707969e9478efcfc13535c
jpcavalcante <jpc.almeida2@gmail.com>
2026-03-08 13:45:10 -0300

    [FTR] CORE - Camada repository para futura implementação de logs.

---
+++ backend/app/Core/Repositories/BaseRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Core\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseRepository implements RepositoryInterface
+{
+    protected Model $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->latest()->get();
+    }
+
+    public function find(int $id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $record = $this->find($id);
+        $record->update($data);
+        return $record;
+    }
+
+    public function delete(int $id)
+    {
+        $record = $this->find($id);
+        return $record->delete();
+    }
+}
